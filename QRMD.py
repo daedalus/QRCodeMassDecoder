91b65b3c4c58e312bd697771f48ab3ae1c56323a
Dario Clavijo <daedalus2027@gmail.com>
2017-06-03 18:33:42 -0300

    rotate the image to scan for qrcodes again

--- QRMD.py
+++ QRMD.py
@@ -6,35 +6,65 @@ import imghdr
 import sys
 import zbar
 import Image
+import ImageFilter
 import os
-import fnmatch
+#import fnmatch
+
+hex_fp = open(sys.argv[1],'rw+')
 
 def scan_image(filename):
 	ret = []
-	# create a reader
-	scanner = zbar.ImageScanner()
 
+	print filename
+
+	# create a reader
 	# configure the reader
+	scanner = zbar.ImageScanner()
 	scanner.parse_config('enable')
 
 	# obtain image data
 	pil = Image.open(filename).convert('L')
-	width, height = pil.size
-	raw = pil.tobytes()
 
-	# wrap image data
-	image = zbar.Image(width, height, 'Y800', raw)
+	def from_pil(scanner,pil):
+		#pil = pil.convert('L')
+		width, height = pil.size
+		raw = pil.tobytes()
 
-	# scan the image for barcodes
-	scanner.scan(image)
+		# wrap image data
+		image = zbar.Image(width, height, 'Y800', raw)
 
-	# extract results
-	for symbol in image:
-	    # do something useful with results
-	    ret.append((symbol.type, symbol.data))
+		# scan the image for barcodes
+		scanner.scan(image)
 
-	# clean up
-	del(image)
+		# extract results
+		for symbol in image:
+		    # do something useful with results
+		    ret.append((symbol.type, symbol.data))
+
+		# clean up
+		#del(image)
+
+	r = from_pil(scanner,pil)
+	if r is not None:
+		ret += r
+		print "Found: Normal"
+
+	sharpen = True
+	if sharpen:
+		pil2 = pil.filter(ImageFilter.SHARPEN)
+		r = from_pil(scanner,pil2)
+		if r is not None:
+			ret += r
+			print "Found: sharpen!"
+
+	rotate = True
+	if rotate:
+		for angle in xrange(0,180):
+			pil3 = pil.rotate(angle, resample=Image.BICUBIC, expand=0)
+			r = from_pil(scanner,pil3)
+			if r is not None:
+				ret += r
+				print "Found: Rotated: %d", angle
 	return ret
 
 def is_image(filename):
@@ -44,20 +74,51 @@ def is_image(filename):
 		return False
 	return (res is not None)
 
-def walk(fdir):
+
+def loadfile2(filename):
+	data = []
+	fp2 = open(filename,'r')
+	for line in fp2:
+		data.append(line.rstrip())
+	fp2.close
+	return data
+
+def savefile(data,filename):
+	fp2 = open(filename,'w')
+	for line in data:
+		fp2.write(line+"\n")
+	fp2.close()
+
+def loadfile(fp):
+        data = []
+        for line in fp:
+                data.append(line.rstrip().decode('hex'))
+        return data
+
+def walk(cache,data,fp,fdir):
 	for root, d, files in os.walk(fdir):
         	#for items in fnmatch.filter(files, "*"):
 		for fname in files:
 			f = root + "/"  + fname
-			if os.path.isfile(f) == True and is_image(f):
-				try:
+			if f not in cache:
+				if os.path.isfile(f) == True and is_image(f):
+					#try:
 					ret = scan_image(f)
 					if ret is not None and len(ret) > 0:
 						for i in ret:
-							print f,i
+							if i[1] not in data:
+								print f,i
+								data.append(i[1])
+								fp.write(i[1].encode('hex')+"\n")
 					#os.flush()
 					sys.stdout.flush()
-				except:
-					pass
+					#except:
+					#	print "error"
+				cache.append(f)
 
-walk(sys.argv[1])
+
+cache = loadfile2('.hash_cache')
+data = loadfile(hex_fp)
+walk(cache,data,hex_fp,sys.argv[2])
+fp.close()
+savefile(cache,'.hash_cache')
